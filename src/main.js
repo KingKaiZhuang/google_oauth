16d60ec53a996befa1ec3c998e5c1137ea9c221e
KingKaiZhuang <5b0g0024@stust.edu.tw>
2024-03-17 16:52:18 +0800

    Add axios and vue3-google-login dependencies, and implement Google login functionality

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import vue3GoogleLogin from "vue3-google-login";
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+
+app.use(router);
+app.use(vue3GoogleLogin, {
+  clientId:
+    "112385572293-6tco996tc532id3pk00o4rrve8eet6sc.apps.googleusercontent.com",
+});
+app.mount("#app");
